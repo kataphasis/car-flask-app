2ee29fc79cf164ef3e7273b63bf5f413b50999b6
kataphasis <kataphasis22@gmail.com>
2022-07-15 14:55:17 -0600

    first commit

---
+++ app/blueprints/main/models.py
@@ -0,0 +1,39 @@
+from app import db
+from datetime import datetime
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), unique=True)
+    username = db.Column(db.String(50))
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    password = db.Column(db.String(100))
+    posts = db.relationship('Post', backref="user", lazy='dynamic')
+
+    def hash_my_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_my_password(self, password):
+        return check_password_hash(self.password, password)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(500))
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def get_user(self):
+        return User.query.get(self.user_id)
+
+class Car(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    make = db.Column(db.String(20))
+    model = db.Column(db.String(25))
+    year = db.Column(db.Integer)
+    color = db.Column(db.String(25))
+    price = db.Column(db.Integer)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
